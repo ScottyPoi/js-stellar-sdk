b8885dcf446c2a3e5f2e290fa4a13b6076445e5d
ScottyPoi <scott.simpson@ethereum.org>
2025-09-05 12:01:23 -0600

    update nodejs test config file

--- test/test-nodejs.ts
+++ test/test-nodejs.ts
@@ -1,17 +1,10 @@
-/* eslint-disable no-undef */
+import { vi } from 'vitest'
+import { expect } from 'vitest'
+import StellarSdk from '../src'
+import axios from 'axios'
 
-require("@babel/register");
-global.StellarSdk = require("../lib");
-
-global.axios = require("axios");
-global.serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc";
-
-var chaiAsPromised = require("chai-as-promised");
-var chaiHttp = require("chai-http");
-global.chai = require("chai");
-global.chai.should();
-global.chai.use(chaiAsPromised);
-global.chai.use(chaiHttp);
-global.expect = global.chai.expect;
-
-global.sinon = require("sinon");
+global.StellarSdk = StellarSdk
+global.axios = axios
+global.serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc"
+global.expect = expect
+global.vi = vi
