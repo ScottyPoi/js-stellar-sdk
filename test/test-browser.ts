830fa2b9ad0bf9e8cbd666e59b6ac6e63a2aa091
ScottyPoi <scott.simpson@ethereum.org>
2025-09-05 12:02:57 -0600

    eslint ignore test config files

--- test/test-browser.ts
+++ test/test-browser.ts
@@ -4,6 +4,6 @@ import StellarSdk from '../src'
 (window as any).axios = StellarSdk.httpClient
 (window as any).HorizonAxiosClient = StellarSdk.Horizon.AxiosClient
 (window as any).SorobanAxiosClient = StellarSdk.Soroban.AxiosClient
-(window as any).serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc"
+(window as any).serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc";
 
 (window as any).vi = vi
