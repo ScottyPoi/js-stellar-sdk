000147b7492d4af1b5b66f10ef539639af20190c
ScottyPoi <scott.simpson@ethereum.org>
2025-09-05 12:29:56 -0600

    update browser_test.ts

--- test/unit/browser_test.ts
+++ test/unit/browser_test.ts
@@ -1,7 +1,10 @@
-describe("Browser version tests", function () {
-  it("lodash is not exported globally", function () {
+import { describe, it, expect } from 'vitest'
+import _ from 'lodash'
+
+describe("Browser version tests",  () => {
+  it("lodash is not exported globally", () => {
     if (typeof window !== "undefined") {
-      expect(typeof _ === "undefined").to.be.true;
+      expect(typeof _ === "undefined").toBeTruthy();
     }
   });
 });
