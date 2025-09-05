0d793c6d1e5930298dd0839e03affb587604563e
ScottyPoi <scott.simpson@ethereum.org>
2025-09-05 12:00:26 -0600

    update test-browser config file

--- test/test-browser.ts
+++ test/test-browser.ts
@@ -1,6 +1,9 @@
-/* eslint-disable no-undef */
-chai.use(require("chai-as-promised"));
-window.axios = StellarSdk.httpClient;
-window.HorizonAxiosClient = StellarSdk.Horizon.AxiosClient;
-window.SorobanAxiosClient = StellarSdk.Soroban.AxiosClient;
-window.serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc";
+import { vi } from 'vitest'
+import StellarSdk from '../src'
+
+(window as any).axios = StellarSdk.httpClient
+(window as any).HorizonAxiosClient = StellarSdk.Horizon.AxiosClient
+(window as any).SorobanAxiosClient = StellarSdk.Soroban.AxiosClient
+(window as any).serverUrl = "https://horizon-live.stellar.org:1337/api/v1/jsonrpc"
+
+(window as any).vi = vi
