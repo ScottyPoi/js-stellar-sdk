2e208e8bf4b04da4f7d32de16592b9067c186493
ScottyPoi <scott.simpson@ethereum.org>
2025-09-05 12:31:50 -0600

    update call_builders_test.ts

--- test/unit/call_builders_test.ts
+++ test/unit/call_builders_test.ts
@@ -1,16 +1,16 @@
 import URI from "urijs";
-// not exported by the library
+import { describe, it, expect } from 'vitest'
 import { CallBuilder } from "../../lib/horizon/call_builder";
 
 describe("CallBuilder functions", function () {
   it("doesn't mutate the constructor passed url argument (it clones it instead)", function () {
     let arg = URI("https://onedom.ain/");
     const builder = new CallBuilder(arg);
-    builder.url.segment("one_segment");
-    builder.checkFilter();
+    builder['url'].segment("one_segment");
+    builder['checkFilter']();
 
     expect(arg.toString()).not.to.be.equal("https://onedom.ain/one_segment"); // https://onedom.ain/
-    expect(builder.url.toString()).to.be.equal(
+    expect(builder['url'].toString()).toEqual(
       "https://onedom.ain/one_segment",
     );
   });
